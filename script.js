b61e6403639d05fc574e48308436be4f3a9e67e1
Karol Jajko <karoljajko@gmail.com>
2024-03-16 19:45:45 +0100

    ready concept carkom side

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-const menuIcons = document.querySelectorAll('.nav__icons__element')
+const menuIcons = document.querySelectorAll('.nav__menu__btn-element')
 const menuNav = document.querySelector('.nav__menu')
 const menuNavItem = document.querySelectorAll('.nav__menu__item')
 
 const switchIconMenu = () => {
-    menuIcons.forEach(icon => {
-        icon.classList.toggle('nav__icons__element-hide')
-    });
-    menuNav.classList.toggle('nav__menu-active')
+	menuNav.classList.toggle('nav__menu-active')
 }
 
-menuIcons.forEach(icon => {
-    icon.addEventListener('click', switchIconMenu)
-});
+menuIcons.forEach(btn => {
+	btn.addEventListener('click', switchIconMenu)
+})
 
-menuNavItem.forEach(item => {
-    item.addEventListener('click', switchIconMenu)
-});
+menuNavItem.forEach(el => {
+	el.addEventListener('click', switchIconMenu)
+})
