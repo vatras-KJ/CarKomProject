db8c605f0d59eb3421dffe86ae07ca0056591953
Karol Jajko <karoljajko@gmail.com>
2024-04-16 08:07:52 +0200

    improving code performance

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const menuIcons = document.querySelectorAll('.nav__menu__btn-element')
 const menuNav = document.querySelector('.nav__menu')
 const menuNavItem = document.querySelectorAll('.nav__menu__item')
+const footerYear = document.querySelector('.footer__year')
 
 const switchIconMenu = () => {
 	menuNav.classList.toggle('nav__menu-active')
@@ -13,3 +14,10 @@ menuIcons.forEach(btn => {
 menuNavItem.forEach(el => {
 	el.addEventListener('click', switchIconMenu)
 })
+
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+handleCurrentYear()
