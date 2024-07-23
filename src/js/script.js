253bb24018a32e6a7206a6819e1be41dba09caef
Karol Jajko <karoljajko@gmail.com>
2024-07-23 08:11:51 +0200

    added cookie

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,10 @@ const menuIcons = document.querySelectorAll('.nav__menu__btn-element')
 const menuNav = document.querySelector('.nav__menu')
 const menuNavItem = document.querySelectorAll('.nav__menu__item')
 const footerYear = document.querySelector('.footer__year')
+const cookieBox = document.querySelector('.cookie')
+const cookieBtn = document.querySelector('.cookie-btn')
+const disclaimerBtn = document.querySelector('.disclaimer-btn')
+const disclaimerText = document.querySelector('.disclaimer-text')
 
 const switchIconMenu = () => {
 	menuNav.classList.toggle('nav__menu-active')
@@ -15,10 +19,30 @@ menuNavItem.forEach(el => {
 	el.addEventListener('click', switchIconMenu)
 })
 
+const showCookie = () => {
+	const cookieEaten = localStorage.getItem('cookie')
+	if (cookieEaten) {
+		cookieBox.classList.add('hide-cookies')
+	}
+}
+
+const handleCookieBox = () => {
+	localStorage.setItem('cookie', 'true')
+	cookieBox.classList.add('hide-cookies')
+}
+
+const handleCookieBoxDisclaimer = () => {
+	disclaimerText.classList.toggle('hide-cookies')
+}
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
 }
+
+cookieBtn.addEventListener('click', handleCookieBox)
+disclaimerBtn.addEventListener('click', handleCookieBoxDisclaimer)
+
+showCookie()
 handleCurrentYear()
 AOS.init();
